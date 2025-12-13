ee4390547bc666e233ef70bef24f2840953e9084
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2025-12-13 17:30:56 -0300

    Initial commit

---
+++ app/src/main/java/com/miliogo/notafiscal/MainActivity.kt
@@ -0,0 +1,164 @@
+package com.miliogo.notafiscal
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.miliogo.notafiscal.ui.theme.NotaFiscalTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent { NotaFiscalTheme { App() } }
+    }
+}
+
+enum class Destination(
+    val route: String,
+    val label: String,
+    val icon: ImageVector,
+    val contentDescription: String
+) {
+    HOME("home", "Home", Icons.Default.Home, "Home"),
+    LOOKUP("lookup", "Consulta", Icons.Default.Search, "Lookup"),
+    ADD("add", "Adicionar", Icons.Default.Add, "Add"),
+    ACCOUNT("account", "Conta", Icons.Default.AccountCircle, "Account")
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun App()
+{
+    val navController = rememberNavController()
+    val startDestination = Destination.HOME
+    var selectedDestination by remember { mutableIntStateOf(startDestination.ordinal) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Nota Fiscal")
+                }
+            )
+        },
+        bottomBar = {
+            NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+                Destination.entries.forEachIndexed { index, destination ->
+                    NavigationBarItem(
+                        selected = selectedDestination == index,
+                        onClick = {
+                            navController.navigate(route = destination.route)
+                            selectedDestination = index
+                        },
+                        icon = {
+                            Icon(
+                                destination.icon,
+                                contentDescription = destination.contentDescription
+                            )
+                        },
+                        label = { Text(destination.label) }
+                    )
+                }
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+
+        AppNavHost(
+            navController = navController,
+            startDestination = startDestination,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier)
+{
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text("Bem vindo!")
+    }
+}
+
+@Composable
+fun LookupScreen(modifier: Modifier = Modifier)
+{
+
+}
+
+@Composable
+fun AddScreen(modifier: Modifier = Modifier)
+{
+    
+}
+
+@Composable
+fun AccountScreen(modifier: Modifier = Modifier)
+{
+
+}
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController,
+    startDestination: Destination,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController,
+        startDestination = startDestination.route
+    ) {
+        Destination.entries.forEach { destination ->
+            composable(destination.route) {
+                when (destination)
+                {
+                    Destination.HOME    -> HomeScreen(modifier)
+                    Destination.LOOKUP  -> LookupScreen(modifier)
+                    Destination.ADD     -> AddScreen(modifier)
+                    Destination.ACCOUNT -> AccountScreen(modifier)
+                }
+            }
+        }
+    }
+}
