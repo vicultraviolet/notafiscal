48eb4ffeb90c6f7db6ef2b4e51c4649f9a3999a2
vicultraviolet <95176727+vicultraviolet@users.noreply.github.com>
2026-05-16 15:32:36 -0300

    Added message in add screen when not currently logged in

--- app/src/main/java/com/miliogo/nfce/screens/AddScreen.kt
+++ app/src/main/java/com/miliogo/nfce/screens/AddScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,30 +25,39 @@ fun AddScreen(
     var shouldScan by remember { mutableStateOf(true) }
     var waitText by remember { mutableStateOf("Processando...") }
 
-    if (shouldScan) {
-        ScanWithPermission(modifier) {
-            if (!shouldScan)
-                return@ScanWithPermission
+    val secretKey by viewModel.secretKey.collectAsState()
 
-            shouldScan = false
-            viewModel.processNFCe(it) { msg ->
-                shouldScan = false
-                waitText = msg
+    if (secretKey.isEmpty()) {
+        shouldScan = false
+        waitText = "Para adicionar um cupom, é necessário ter uma conta: Configurações -> Minha conta"
+    }
 
-                delay(3.seconds)
-
-                shouldScan = true
-                waitText = "Processando..."
-            }
-        }
-    } else
-    {
+    if (!shouldScan) {
         Column(
             modifier = modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
         ) {
             Text(waitText)
+        }
+
+        return
+    }
+
+
+    ScanWithPermission(modifier) {
+        if (!shouldScan)
+            return@ScanWithPermission
+
+        shouldScan = false
+        viewModel.processNFCe(it) { msg ->
+            shouldScan = false
+            waitText = msg
+
+            delay(3.seconds)
+
+            shouldScan = true
+            waitText = "Processando..."
         }
     }
 }
